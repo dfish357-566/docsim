ff210083d2c1fa521da5fa19ee929c773416a7bd
David Fisher <78123856+dfish357-566@users.noreply.github.com>
2022-06-04 12:22:19 -0400

    Update accounting_nlp.py

--- accounting_nlp.py
+++ accounting_nlp.py
@@ -40,3 +40,17 @@ if uploaded_files:
      # Change the row indexes
      df.index = df_labels
      st.table(df)
+     
+     @st.cache
+      def convert_df(df):
+          # IMPORTANT: Cache the conversion to prevent computation on every rerun
+          return df.to_csv().encode('utf-8')
+
+     csv = convert_df(my_large_df)
+
+     st.download_button(
+          label="Download data as CSV",
+          data=csv,
+          file_name='large_df.csv',
+          mime='text/csv',
+      )
